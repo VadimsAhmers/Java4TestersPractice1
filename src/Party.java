d89beeaf6f3762b271be7ea13cf96d30b0d6962a
Вадим <vadimsky02@gmail.com>
2016-12-18 21:58:07 +0500

    Class Product filling

---
+++ src/Party.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Вадим on 18.12.2016.
+ */
+public class Party {
+
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+
+}
--- src/Product.java
+++ src/Product.java
@@ -7,5 +7,34 @@ public class Product {
     private double price;
     private int quantity;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+
+
+
+
 
 }
